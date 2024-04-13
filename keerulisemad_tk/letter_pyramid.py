5a4d38e9d98423ef096906b12096fe023604ae01
geveev <144543740+geveev@users.noreply.github.com>
2024-04-13 07:35:32 -0700

    Add files via upload

--- keerulisemad_tk/letter_pyramid.py
+++ keerulisemad_tk/letter_pyramid.py
@@ -1,25 +1,30 @@
 """KT tähtede püramiid -- geveev"""
 
 
-def letter_triangle(letters):
+def letter_pyramid(letters):
     """
     Generate a triangle pattern using letters from the input string. Letters must have spaces between and
     last row can be incomplete if there are not enough letters.
 
     For example:
     letters("tere õhtust") ==
-                    "         t \n"
-                    "        e r \n"
-                    "       e õ h \n"
-                    "      t u s t"
+    "   t\n"
+    "  e r\n"
+    " e õ h\n"
+    "t u s t"
 
 
     letters("tere hommikust") ==
-                    "            T \n"
-                    "           e r \n"
-                    "          e H o \n"
-                    "         m m i k \n"
-                    "        u s t"
+    "    t\n"
+    "   e r\n"
+    "  e h o\n"
+    " m m i k\n"
+    "u s t"
+
+    letters("12512") ==
+    "  1\n"
+    " 2 5\n"
+    "1 2"
 
     :param letters:
     :return pyramid of letters:
@@ -30,12 +35,21 @@ def letter_triangle(letters):
     n = len(letters)
     index = 0
     row_length = 1
+    rows = []
+
+    # Kõik read
     while index < n:
-        triangle += " " * (n - row_length)
-        for _ in range(row_length):
-            if index < n:
-                triangle += letters[index] + " "
-                index += 1
-        triangle += "\n"
+        end_index = min(index + row_length, n)
+        rows.append(letters[index:end_index])
+        index += row_length
         row_length += 1
-    return triangle
+
+    total_rows = len(rows)
+
+    # Püramiidi ehitaja
+    for i, row in enumerate(rows):
+        leading_spaces = total_rows - 1 - i
+        line = " " * leading_spaces + " ".join(row)
+        triangle += line + "\n"
+
+    return triangle.rstrip()
